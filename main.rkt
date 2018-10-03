a591ecb6671c0314aae44997b0a7dc5ea0185198
Stephen <stephen@thoughtstem.com>
2018-10-03 12:20:00 -0700

    adding main

---
+++ main.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(provide
+ (all-from-out "half-sheets.rkt")
+ (all-from-out "fleet-winter-2017.rkt")
+
+ #%module-begin)
+
+(require "half-sheets.rkt")
+(require "fleet-winter-2017.rkt")

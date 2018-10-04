0751457dce3f4493046a16da0f6c35a38cdecb1b
srfoster <stephen@thoughtstem.com>
2018-10-04 15:50:05 -0700

    ts-racket deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang info
 
 (define deps '(
-               "https://github.com/thoughtstem/magic-loader.git"))
+               "https://github.com/thoughtstem/magic-loader.git"
+               "https://github.com/thoughtstem/ts-racket.git"
+               ))
